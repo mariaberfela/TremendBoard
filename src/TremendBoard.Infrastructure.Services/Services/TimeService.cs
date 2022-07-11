2dd530345418f8a983479c109cb6d06fec3e9499
alex-sasu-tremend <alexandru.sasu@tremend.com>
2022-07-11 21:56:37 +0300

    Dependency Injection homework

---
+++ src/TremendBoard.Infrastructure.Services/Services/TimeService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TremendBoard.Infrastructure.Services.Interfaces;
+
+namespace TremendBoard.Infrastructure.Services.Services
+{
+    public class TimeService: ITimeService
+    {
+        private String _time;
+
+        public TimeService()
+        {
+            
+            _time = DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss.fff");
+        }
+
+        public String GetCurrentTime()
+        {
+            return _time;
+        }
+    }
+}
--- src/TremendBoard.Mvc/TremendBoard.Mvc/Controllers/HomeController.cs
+++ src/TremendBoard.Mvc/TremendBoard.Mvc/Controllers/HomeController.cs
@@ -8,16 +8,25 @@ namespace TremendBoard.Mvc.Controllers
     public class HomeController : Controller
     {
         private readonly IDateTime _dateTime;
+        private readonly ITimeService _timeService1;
+        private readonly ITimeService _timeService2;
 
-        public HomeController(IDateTime dateTime)
+        public HomeController(IDateTime dateTime,
+            ITimeService timeService1,
+            ITimeService timeService2)
         {
             _dateTime = dateTime;
+            _timeService1 = timeService1;
+            _timeService2 = timeService2;
         }
 
         public IActionResult Index()
         {
+            ViewData["timeService1"] = _timeService1.GetCurrentTime();
+            ViewData["timeService2"] = _timeService2.GetCurrentTime();
+
             var serverTime = _dateTime.Now;
-            
+
             if (serverTime.Hour < 12)
             {
                 ViewData["Message"] = "It's morning here - Good Morning!";
