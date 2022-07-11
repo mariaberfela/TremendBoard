00b3d5114b5c2b3e6c0e79b37cb9921fad319ddb
DESKTOP-5C2I9LU\alexandru.carpinean <alexandru.carpineanu@tremend.com>
2022-07-11 19:35:43 +0300

    DependencyInjection Task

--- src/TremendBoard.Mvc/TremendBoard.Mvc/Controllers/HomeController.cs
+++ src/TremendBoard.Mvc/TremendBoard.Mvc/Controllers/HomeController.cs
@@ -8,14 +8,22 @@ namespace TremendBoard.Mvc.Controllers
     public class HomeController : Controller
     {
         private readonly IDateTime _dateTime;
+        private readonly ITimeService _timeService1;
+        private readonly ITimeService _timeService2;
 
-        public HomeController(IDateTime dateTime)
+
+        public HomeController(IDateTime dateTime, ITimeService ts1, ITimeService ts2)
         {
             _dateTime = dateTime;
+            _timeService1 = ts1;
+            _timeService2 = ts2;   
         }
 
         public IActionResult Index()
         {
+            ViewData["timeService1"] = _timeService1.GetCurrentTime();
+            ViewData["timeService2"] = _timeService2.GetCurrentTime();
+
             var serverTime = _dateTime.Now;
             
             if (serverTime.Hour < 12)
