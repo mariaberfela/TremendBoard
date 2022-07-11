57ac4697846d968a71ddef432381d3b56b7f8d24
DESKTOP-UJ0IL9E\stefan.grigorescu <stefan.grigorescu@tremend.com>
2022-07-11 17:27:26 +0300

    Dependency Injection task; Tested Transient, Scoped and Singleton lifetimes behavior.

---
+++ src/TremendBoard.Infrastructure.Services/Services/TimeService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TremendBoard.Infrastructure.Services.Interfaces;
+
+namespace TremendBoard.Infrastructure.Services.Services
+{
+    public class TimeService: ITimeService
+    {
+        public string time { get; set; }
+
+
+        public TimeService() {
+            time = DateTime.Now.ToString("hh:mm:ss.fff");
+        }
+        
+        public string GetCurrentTime() => time;
+    }
+}
