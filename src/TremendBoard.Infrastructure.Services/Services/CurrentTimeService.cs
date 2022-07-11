5c9ea089ed7543a1a8b37d3f8ba8e10e4c194d4c
Sebastian Nedelcu <sebastian.nedelcu@tremend.com>
2022-07-11 18:31:01 +0300

    Added ITimeService Interface Added instances of ITimeService in HomeController, Index action and View Added Files/DependencyInjectionFindings.txt

---
+++ src/TremendBoard.Infrastructure.Services/Services/CurrentTimeService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TremendBoard.Infrastructure.Services.Interfaces;
+
+namespace TremendBoard.Infrastructure.Services.Services
+{
+    public class CurrentTimeService : ITimeService
+    {
+        public DateTime GetCurrentTime { get; private set; }
+        public string GetGUID { get; private set; }
+
+        public CurrentTimeService()
+        {
+            GetCurrentTime = DateTime.Now;
+            GetGUID = Guid.NewGuid().ToString(); 
+        }
+    }
+}
