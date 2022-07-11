bb10482d4c2940464f112874bd608ef609c4891e
alex-sasu-tremend <alexandru.sasu@tremend.com>
2022-07-11 23:00:30 +0300

    Logging homework

--- src/TremendBoard.Mvc/TremendBoard.Mvc/Controllers/HomeController.cs
+++ src/TremendBoard.Mvc/TremendBoard.Mvc/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Diagnostics;
 using TremendBoard.Infrastructure.Services.Interfaces;
 using TremendBoard.Mvc.Models;
@@ -8,10 +10,13 @@ namespace TremendBoard.Mvc.Controllers
     public class HomeController : Controller
     {
         private readonly IDateTime _dateTime;
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(IDateTime dateTime)
+        public HomeController(IDateTime dateTime,
+            ILogger<HomeController> logger)
         {
             _dateTime = dateTime;
+            _logger = logger;
         }
 
         public IActionResult Index()
@@ -31,6 +36,30 @@ namespace TremendBoard.Mvc.Controllers
                 ViewData["Message"] = "It's evening here - Good Evening!";
             }
 
+            try
+            {
+                for (int i = 60; i < 80; i++)
+                {
+                    if (i == 69)
+                    {
+                        _logger.LogWarning("Very dangerous number");
+                    }
+                    else if (i == 79)
+                    {
+                        throw new Exception("End of for loop");
+                    }
+                    else
+                    {
+                        _logger.LogInformation("The value of i is {Variable}", i);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogInformation(e.StackTrace);
+                _logger.LogError("An error was caught.");
+            }
+
             return View();
         }
 
@@ -38,6 +67,12 @@ namespace TremendBoard.Mvc.Controllers
         {
             ViewData["Message"] = "Currently on the server the time is " + dateTime.Now;
 
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            _logger.LogWarning("You accessed the Privacy page!");
             return View();
         }
 
