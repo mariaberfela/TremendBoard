f02e7b19485eaf04033e99c1f47e5b0f20cccd9c
anamaria2602 <ana.avram@tremend.com>
2022-07-12 10:03:22 +0300

    Tema

---
+++ src/TremendBoard.Infrastructure.Services/Services/TimeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TremendBoard.Infrastructure.Services.Interfaces;
+
+namespace TremendBoard.Infrastructure.Services.Services
+{
+    internal class TimeService : ITimeService
+    {
+        public string GetCurrentTime()
+        {
+            string time = DateTime.Now.ToString("h:mm:ss tt");
+            return time;
+        }
+       
+    }
+}
