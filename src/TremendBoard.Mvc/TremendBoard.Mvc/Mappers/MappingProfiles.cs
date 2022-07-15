d053541e4a386ea03f45fe9923bd6b95beb8f051
alex-sasu-tremend <109100500+alex-sasu-tremend@users.noreply.github.com>
2022-07-15 14:55:19 +0300

    Added and utilized more AutoMapper mapping profiles

---
+++ src/TremendBoard.Mvc/TremendBoard.Mvc/Mappers/MappingProfiles.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using TremendBoard.Infrastructure.Data.Models;
+using TremendBoard.Infrastructure.Data.Models.DTOs;
+using TremendBoard.Mvc.Models.ProjectViewModels;
+using TremendBoard.Mvc.Models.RoleViewModels;
+using TremendBoard.Mvc.Models.UserViewModels;
+
+namespace TremendBoard.Mvc.Mappers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<ProjectDetailViewModel, Project>();
+            CreateMap<ProjectDetailViewModel, ProjectDTO>();
+
+            CreateMap<ProjectUserDTO, ProjectUserDetailViewModel>();
+            CreateMap<UserDTO, UserDetailViewModel>().
+                ForMember(user => user.ApplicationRoles, opt => opt.Ignore());
+            CreateMap<ApplicationRoleDTO, ApplicationRoleDetailViewModel>();
+            CreateMap<ProjectDTO, ProjectDetailViewModel>();
+        }
+    }
+}
