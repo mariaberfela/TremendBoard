0aae0a2c1aafd02614fb62f7dafbfbdb36f1d209
Radu Matei <radu.matei@tremend.com>
2022-07-18 10:09:05 +0300

    Added 2 more unit tests

---
+++ src/TremendBoard.Mvc/TremendBoard.Mvc/Multiplication.cs
@@ -0,0 +1,29 @@
+﻿namespace TremendBoard.Mvc
+{
+    public class Multiplication
+    {
+        private int _num1;
+        private int _num2;
+
+        public Multiplication(int num1, int num2)
+        {
+            _num1 = num1;
+            _num2 = num2;
+        }
+
+        public int GetMult()
+        {
+            return _num1 * _num2;
+        }
+
+        public bool AreEqual()
+        {
+            if(_num1 == _num2)
+            {
+                return true;
+
+            }
+            return false;
+        }
+    }
+}
---
+++ src/TremendBoard.Mvc/XUnitTests/UnitTest3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TremendBoard.Mvc;
+
+namespace XUnitTests
+{
+    public class UnitTest3
+    {
+        [Fact]
+        public void Multiplication_ReturnsMultiplicationOfAAndB_EqualToATimesB()
+        {
+            int num1 = 2;
+            int num2 = 3;
+
+            Multiplication mult = new Multiplication(num1, num2);
+
+            int actual = mult.GetMult();
+            Assert.Equal(num1*num2, actual);
+        }
+
+        [Fact]
+        public void Multiplication_ReturnsIfAEqualToB_ReturnsTrueOrFalse()
+        {
+            int num1 = 2;
+            int num2 = 2;
+
+            Multiplication mult = new Multiplication(num1, num2);
+
+            bool equal = true;
+
+            bool actual = mult.AreEqual();
+            Assert.Equal(equal, actual);
+        }
+    }
+}
