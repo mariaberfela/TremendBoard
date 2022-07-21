9ecb7e02cea36d67b1bb8036446d4f55bd043305
onumirea <oana.mirea@tremend.com>
2022-07-21 15:19:05 +0300

    Unit Testing Task

---
+++ src/TremendBoard.Mvc/UnitTests/MoqTests/UnitTest4.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TremendBoard.Infrastructure.Data.Models;
+using TremendBoard.Infrastructure.Services.Interfaces;
+using TremendBoard.Mvc.Controllers;
+
+namespace ProjectServiceTests.MoqTests
+{
+    public class UnitTest4
+    {
+        [Fact]
+        public void Get_Current_Time()
+        {
+
+            var _mockTimeService = new Mock<ITimeService>();
+
+            _mockTimeService
+                .Setup(x => x.GetCurrentTime)
+            .Returns(DateTime.Now);
+
+
+            // Act
+            var returnedTime = _mockTimeService.Object.GetCurrentTime.ToString("yyyy-mm-dd hh:mm");
+            var expectedTime = DateTime.Now.ToString("yyyy-mm-dd hh:mm");
+            // Assert
+            Assert.Equal(returnedTime, expectedTime);
+
+        }
+    }
+}
---
+++ src/TremendBoard.Mvc/UnitTests/XunitTests/UnitTest2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TremendBoard.Infrastructure.Services.Services;
+
+namespace ProjectServiceTests.XunitTests
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public void TimeServiceDayOfWeek_returns_current_time_day_of_week()
+        {
+
+            TimeService _timeService = new TimeService();
+
+            // Act
+            var returnedTime = _timeService.DayOfWeek;
+            var expectedTime = DateTime.Now.DayOfWeek.ToString();
+            // Assert
+            Assert.Equal(returnedTime, expectedTime);
+
+        }
+    }
+}
